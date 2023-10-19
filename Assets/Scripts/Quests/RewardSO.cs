5ba26dcacb6a00115a3c72e060b39692a68d91f1
unknown <natasha.pechenka@gmail.com>
2023-10-19 15:21:59 +0600

    Enum Weapons

---
+++ Assets/Scripts/Quests/RewardSO.cs
@@ -0,0 +1,17 @@
+using System.Collections.Concurrent;
+using System.IO.Enumeration;
+using UnityEngine;
+
+[CreateAssetMenu(
+    fileName = "Reward Item",
+    menuName = "RPG/Reward Item SO",
+    order = 2
+)]
+public class RewardSO : ScriptableObject
+{
+    public float bonusHealth = 0f;
+    public float bonusDamage = 0f;
+    public int bonusPotions = 0;
+    public bool forceWeaponSwap = false;
+    public Weapons weapon = Weapons.Sword;
+}
